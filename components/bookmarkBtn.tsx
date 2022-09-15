187c5eceed0e75e900becea6fc91697fba958e79
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-15 22:05:54 +0900

    💄 bookmarkBtn 컴포넌트 생성

---
+++ components/bookmarkBtn.tsx
@@ -0,0 +1,9 @@
+import { BookmarkIcon } from '@heroicons/react/24/outline';
+
+export default function BookmarkBtn() {
+  return (
+    <button type="button">
+      <BookmarkIcon className="w-6 h-6 text-gray-500 ml-2" />
+    </button>
+  );
+}
