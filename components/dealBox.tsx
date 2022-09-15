2c949d8cd6d2fef78ad0f7f896b65d0fb8bcf0d1
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-15 22:05:26 +0900

    💄 dealBox 컴포넌트 생성

---
+++ components/dealBox.tsx
@@ -0,0 +1,38 @@
+import { StaticImageData } from 'next/image';
+import Link from 'next/link';
+import BookmarkBtn from './bookmarkBtn';
+import HeartBtn from './heartBtn';
+
+interface DealBoxProps {
+  product: {
+    id: number;
+    name: string;
+    price: string;
+    imageSrc: StaticImageData;
+  };
+}
+
+export default function DealBox({ product }: DealBoxProps) {
+  return (
+    <Link key={product.id} href={`/deal/${product.id}`}>
+      <div className="w-full">
+        <div className="w-full h-[220px] overflow-hidden rounded-sm">
+          <div
+            style={{
+              backgroundImage: `url(${product.imageSrc.src})`,
+            }}
+            className="h-full bg-center bg-cover"
+          ></div>
+        </div>
+        <div className="mt-3 flex items-center justify-between text-base text-gray-900">
+          <h3>{product.name}</h3>
+          <div className="flex items-center">
+            <HeartBtn likes={13} />
+            <BookmarkBtn />
+          </div>
+        </div>
+        <p className="mt-1  font-medium">{product.price}</p>
+      </div>
+    </Link>
+  );
+}
