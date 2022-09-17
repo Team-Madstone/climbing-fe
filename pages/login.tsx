2329bcfebf69f28797870d5c6af68c63036d0869
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-18 01:50:39 +0900

    ✨ react-hook-form 및 에러 적용

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,12 @@
-import { gql, useMutation, useReactiveVar } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
-import {
-  isClientSide,
-  isLoginVar,
-  isServerError,
-  LOGIN_TOKEN,
-} from '../apollo-store';
+import React, { useEffect } from 'react';
+import { handleLogin, isServerError } from '../apollo-store';
 import Layout from '../components/layout';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 const LOGIN_MUTATION = gql`
   mutation login($email: String!, $password: String!) {
@@ -23,60 +19,37 @@ const LOGIN_MUTATION = gql`
   }
 `;
 
+interface FormInput {
+  email: String;
+  password: String;
+}
+
 const Login: NextPage = () => {
   const router = useRouter();
-  const isLogin = useReactiveVar(isLoginVar);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [isError, setIsError] = useState(false);
-
-  const handleLogin = (token: string) => {
-    isClientSide && localStorage.setItem(LOGIN_TOKEN, token);
-    isLoginVar(true);
-  };
-
-  const handleLogout = () => {
-    isClientSide && localStorage.removeItem(LOGIN_TOKEN);
-    isLoginVar(false);
-  };
-
-  const [loginMutaion, { loading, error, data }] = useMutation(LOGIN_MUTATION, {
-    onError: (error) => {
-      const networkError = error.networkError;
-      isServerError(networkError) && console.log(networkError.result);
-      if (isServerError(networkError) && networkError.result.status === false) {
-        setIsError(true);
-      }
-    },
-    onCompleted: (data) => {
-      console.log(data, 'data');
-      router.push('/');
-    },
-  });
+  const { register, handleSubmit, getValues, watch } = useForm<FormInput>();
+  const [loginMutaion, { loading, data, error, reset }] = useMutation(
+    LOGIN_MUTATION,
+    {
+      onError: (error) => undefined,
+    }
+  );
 
   useEffect(() => {
     if (data?.loginResult?.token) {
-      console.log(data, 'data');
       handleLogin(data.loginResult.token);
+      router.push('/');
     }
-  }, [data]);
+  }, [data, router]);
 
-  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = e;
-    setEmail(value);
-  };
+  const email = watch('email');
+  const password = watch('password');
 
-  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = e;
-    setPassword(value);
-  };
+  useEffect(() => {
+    reset();
+  }, [email, password, reset]);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleFormSubmit: SubmitHandler<FormInput> = () => {
+    const { email, password } = getValues();
     !loading &&
       loginMutaion({
         variables: {
@@ -86,7 +59,6 @@ const Login: NextPage = () => {
       });
   };
 
-  console.log(loading, 'loading');
   return (
     <Layout title="로그인" hasTabBar>
       <Head>
@@ -96,7 +68,10 @@ const Login: NextPage = () => {
         <h2 className="text-2xl font-bold text-center text-gray-800">로그인</h2>
         <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white p-4 sm:px-10">
-            <form className="space-y-6" onSubmit={handleSubmit}>
+            <form
+              className="space-y-6"
+              onSubmit={handleSubmit(handleFormSubmit)}
+            >
               <div>
                 <label
                   htmlFor="email"
@@ -106,11 +81,10 @@ const Login: NextPage = () => {
                 </label>
                 <div className="mt-1">
                   <input
+                    {...register('email')}
                     id="email"
                     name="email"
                     type="email"
-                    value={email}
-                    onChange={handleChangeEmail}
                     required
                     className="block w-full appearance-none rounded-sm border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
@@ -126,11 +100,10 @@ const Login: NextPage = () => {
                 </label>
                 <div className="mt-1">
                   <input
+                    {...register('password')}
                     id="password"
                     name="password"
                     type="password"
-                    value={password}
-                    onChange={handleChangePassword}
                     required
                     className="block w-full appearance-none rounded-sm border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
@@ -160,10 +133,9 @@ const Login: NextPage = () => {
                   </button>
                 </Link>
               </div>
-              {isError && (
+              {error && (
                 <p className="text-red-600 text-sm">
-                  {error &&
-                    isServerError(error.networkError) &&
+                  {isServerError(error.networkError) &&
                     error.networkError.result.message}
                 </p>
               )}
