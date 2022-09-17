0b977fd175af233f1e94262724ce23bd2998fa89
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-18 01:51:27 +0900

    ✨ react-hook-form 및 에러 적용

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,8 +1,81 @@
+import { gql, useMutation } from '@apollo/client';
 import { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { handleLogin, isServerError } from '../apollo-store';
 import Layout from '../components/layout';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+const returnUrl = 'http://localhost:3000/';
+
+const REGISTER_MUTATION = gql`
+  mutation register(
+    $name: String!
+    $email: String!
+    $password: String!
+    $returnUrl: String!
+  ) {
+    registerResult(
+      input: {
+        name: $name
+        email: $email
+        password: $password
+        returnUrl: $returnUrl
+      }
+    ) @rest(method: "POST", path: "register") {
+      status
+      message
+      token
+    }
+  }
+`;
+
+interface FormInput {
+  email: String;
+  password: String;
+  name: String;
+}
 
 const Register: NextPage = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { isValid, errors: formErrors },
+  } = useForm<FormInput>({
+    mode: 'onChange',
+  });
+
+  const [registerMutation, { loading, error: serverError, data }] = useMutation(
+    REGISTER_MUTATION,
+    {
+      onError: (error) => undefined,
+    }
+  );
+
+  const handleFormSubmit: SubmitHandler<FormInput> = () => {
+    const { email, password, name } = getValues();
+    !loading &&
+      registerMutation({
+        variables: {
+          email,
+          password,
+          name,
+          returnUrl,
+        },
+      });
+  };
+
+  useEffect(() => {
+    if (data?.registerResult?.token) {
+      handleLogin(data.registerResult.token);
+      router.push('/verify-email');
+    }
+  }, [data, router]);
+
   return (
     <Layout title="회원가입" hasTabBar>
       <Head>
@@ -14,7 +87,10 @@ const Register: NextPage = () => {
         </h2>
         <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white p-4 sm:px-10">
-            <form className="space-y-6">
+            <form
+              className="space-y-6"
+              onSubmit={handleSubmit(handleFormSubmit)}
+            >
               <div>
                 <label
                   htmlFor="email"
@@ -24,6 +100,14 @@ const Register: NextPage = () => {
                 </label>
                 <div className="mt-1">
                   <input
+                    {...register('email', {
+                      required: '이메일은 필수 입력 사항입니다.',
+                      pattern: {
+                        value:
+                          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+                        message: '이메일의 형식이 올바르지 않습니다.',
+                      },
+                    })}
                     id="email"
                     name="email"
                     type="email"
@@ -31,6 +115,18 @@ const Register: NextPage = () => {
                     className="block w-full appearance-none rounded-sm border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
+                {formErrors.email?.message && (
+                  <span className="text-sm text-red-600">
+                    {formErrors.email?.message}
+                  </span>
+                )}
+                {serverError &&
+                  isServerError(serverError.networkError) &&
+                  serverError.networkError.result.errors.email && (
+                    <span className="text-sm text-red-600">
+                      {serverError.networkError.result.errors.email}
+                    </span>
+                  )}
               </div>
 
               <div>
@@ -42,6 +138,13 @@ const Register: NextPage = () => {
                 </label>
                 <div className="mt-1">
                   <input
+                    {...register('password', {
+                      required: '비밀번호는 필수 입력 사항입니다.',
+                      minLength: {
+                        value: 8,
+                        message: '비밀번호는 최소 8자 이상입니다.',
+                      },
+                    })}
                     id="password"
                     name="password"
                     type="password"
@@ -49,33 +152,80 @@ const Register: NextPage = () => {
                     className="block w-full appearance-none rounded-sm border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
+                {formErrors.password?.message && (
+                  <span className="text-sm text-red-600">
+                    {formErrors.password?.message}
+                  </span>
+                )}
+                {serverError &&
+                  isServerError(serverError.networkError) &&
+                  serverError.networkError.result.errors.password && (
+                    <span className="text-sm text-red-600">
+                      {serverError.networkError.result.errors.password}
+                    </span>
+                  )}
               </div>
 
               <div>
                 <label
-                  htmlFor="nickname"
+                  htmlFor="name"
                   className="block text-sm font-medium text-gray-700"
                 >
                   닉네임
                 </label>
                 <div className="mt-1">
                   <input
-                    id="nickname"
-                    name="nickname"
-                    type="nickname"
+                    {...register('name', {
+                      required: '닉네임은 필수 입력 사항입니다.',
+                      minLength: {
+                        value: 2,
+                        message: '닉네임은 최소 2글자 이상 입니다.',
+                      },
+                      maxLength: {
+                        value: 20,
+                        message: '닉네임은 최대 20글자까지 가능합니다.',
+                      },
+                      pattern: {
+                        value: /^[가-힣|a-z]+$/,
+                        message: '한글 또는 영어 소문자만 입력 가능합니다.',
+                      },
+                    })}
+                    id="name"
+                    name="name"
+                    type="name"
                     required
                     className="block w-full appearance-none rounded-sm border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
+                {formErrors.name?.message && (
+                  <span className="text-sm text-red-600">
+                    {formErrors.name?.message}
+                  </span>
+                )}
+                {serverError &&
+                  isServerError(serverError.networkError) &&
+                  serverError.networkError.result.errors.name && (
+                    <span className="text-sm text-red-600">
+                      {serverError.networkError.result.errors.name}
+                    </span>
+                  )}
               </div>
 
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-sm border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="flex w-full justify-center rounded-sm border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-indigo-200"
+                  disabled={loading || !isValid}
                 >
-                  회원가입
+                  {loading ? '요청중' : '회원가입'}
                 </button>
+              </div>
+              <div className="flex justify-center">
+                <Link href="/login">
+                  <button className="text-sm text-gray-500 hover:text-gray-700">
+                    로그인
+                  </button>
+                </Link>
               </div>
             </form>
           </div>
