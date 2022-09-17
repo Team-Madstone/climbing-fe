b6a2603c77008245fd8b23c08e2bf85a62b57893
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-18 01:52:39 +0900

    ✨ verify-email 페이지 추가

---
+++ pages/verify-email/index.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from 'next';
+import Link from 'next/link';
+
+const VerifyEmail: NextPage = () => {
+  return (
+    <div className="my-28 mx-6">
+      <p className="text-2xl text-bold">회원가입 완료</p>
+      <p className="mt-4">
+        회원가입이 완료되었습니다.
+        <br />
+        가입한 이메일로 인증을 완료해주세요.
+      </p>
+      <Link href={'/'}>
+        <span className="inline-block bg-indigo-600 px-5 py-3 rounded-sm text-sm text-white mt-16">
+          홈으로 가기
+        </span>
+      </Link>
+    </div>
+  );
+};
+
+export default VerifyEmail;
