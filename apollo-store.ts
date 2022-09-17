738d1a502ed5ef33721ff9f09f267ec67e1e954b
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-18 01:52:16 +0900

    ✨ handleLogin, handleLogout 추가

--- apollo-store.ts
+++ apollo-store.ts
@@ -8,6 +8,16 @@ export const isLoginVar = makeVar(
   isClientSide && Boolean(localStorage.getItem(LOGIN_TOKEN))
 );
 
+export const handleLogin = (token: string) => {
+  isClientSide && localStorage.setItem(LOGIN_TOKEN, token);
+  isLoginVar(true);
+};
+
+export const handleLogout = () => {
+  isClientSide && localStorage.removeItem(LOGIN_TOKEN);
+  isLoginVar(false);
+};
+
 export const isServerError = (
   error: Error | ServerParseError | ServerError | null
 ): error is ServerError => {
