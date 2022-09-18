c7a01cf36afa6a76e45217751142d51db51f7079
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-19 01:37:41 +0900

    ✨ 로그인 여부, 이메일 인증 여부에 따라 라우트 나누기

---
+++ components/authGuard.tsx
@@ -0,0 +1,41 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import useMyProfile from '../hooks/useMyProfile';
+import Loading from './loading';
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default function AuthGuard({ children }: IProps) {
+  const router = useRouter();
+  const { isClientSide, hasLoginToken, user, isFetched } = useMyProfile();
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    if (!isClientSide) return;
+
+    if (hasLoginToken) {
+      if (isFetched) {
+        if (user) {
+          if (!user.email_verified_at) {
+            router.push('/check-verify-email');
+            return;
+          } else {
+            setInit(true);
+            return;
+          }
+        } else {
+          router.push('/login');
+          return;
+        }
+      } else {
+        return;
+      }
+    }
+    router.push('/login');
+  }, [isClientSide, user, isFetched, router, hasLoginToken]);
+  console.log(isClientSide, hasLoginToken, isFetched, user);
+
+  return !init ? <Loading /> : <>{children}</>;
+}
