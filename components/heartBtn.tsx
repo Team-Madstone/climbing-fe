50d3324c14b293727553d7baedbca088631f6940
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-26 16:14:55 +0900

    ✨ 좋아요 버튼 클릭 시 로그인 및 인증된 유저 체크해서 라우터 나누기

--- components/heartBtn.tsx
+++ components/heartBtn.tsx
@@ -1,16 +1,41 @@
+import { useReactiveVar } from '@apollo/client';
 import { HeartIcon } from '@heroicons/react/24/outline';
+import { useRouter } from 'next/router';
+import { loginUserVar } from '../apollo-store';
 
 interface HeartBtnProps {
   likes: number;
 }
 
 export default function HeartBtn({ likes }: HeartBtnProps) {
+  const loginUser = useReactiveVar(loginUserVar);
+  const router = useRouter();
+
+  const checkEmailVerified = () => {
+    if (loginUser) {
+      if (loginUser.email_verified_at) {
+        // 좋아요 기능
+        return;
+      } else {
+        router.push('/check-verify-email');
+        return;
+      }
+    } else {
+      router.push('/login');
+      return;
+    }
+  };
+
   return (
     <div className="flex">
-      <button type="button" className="flex items-center text-gray-500">
+      <button
+        onClick={checkEmailVerified}
+        type="button"
+        className="flex items-center text-gray-500"
+      >
         <HeartIcon className="w-6 h-6 mr-1" />
       </button>
-      <span className="cursor-default">{likes}</span>
+      <span className="cursor-default text-gray-500">{likes}</span>
     </div>
   );
 }
