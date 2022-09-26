de601b5bbb1dab177f11c1d41a74935ba9411ccc
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-26 16:14:49 +0900

    ✨ 북마크 버튼 클릭 시 로그인 및 인증된 유저 체크해서 라우터 나누기

--- components/bookmarkBtn.tsx
+++ components/bookmarkBtn.tsx
@@ -1,8 +1,28 @@
+import { useReactiveVar } from '@apollo/client';
 import { BookmarkIcon } from '@heroicons/react/24/outline';
+import { useRouter } from 'next/router';
+import { loginUserVar } from '../apollo-store';
 
 export default function BookmarkBtn() {
+  const loginUser = useReactiveVar(loginUserVar);
+  const router = useRouter();
+
+  const checkEmailVerified = () => {
+    if (loginUser) {
+      if (loginUser.email_verified_at) {
+        // 북마크 기능
+        return;
+      } else {
+        router.push('/check-verify-email');
+        return;
+      }
+    } else {
+      router.push('/login');
+      return;
+    }
+  };
   return (
-    <button type="button">
+    <button onClick={checkEmailVerified} type="button">
       <BookmarkIcon className="w-6 h-6 text-gray-500 ml-2" />
     </button>
   );
