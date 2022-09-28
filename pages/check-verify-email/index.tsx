e3c6695ff80a34cd3e4f397a589aa87f02dcf9c0
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-29 00:15:17 +0900

    🎨 Anchor 컴포넌트 추가

--- pages/check-verify-email/index.tsx
+++ pages/check-verify-email/index.tsx
@@ -1,11 +1,11 @@
 import { gql, useMutation, useReactiveVar } from '@apollo/client';
 import { NextPage } from 'next';
-import Link from 'next/link';
 import {
   hasLoginTokenVar,
   isClientSideVar,
   LOGIN_TOKEN,
 } from '../../apollo-store';
+import Anchor from '../../components/Anchor';
 import Button from '../../components/Button';
 import { returnUrl } from '../../shared/constances';
 
@@ -51,22 +51,17 @@ const CheckVerifyEmail: NextPage = () => {
   return (
     <div className="max-w-[640px] mx-auto px-4 py-28 sm:px-0">
       <p className="text-2xl text-bold">이메일 인증이 필요합니다.</p>
-      <p className="mt-4">
+      <p className="my-4">
         해당 서비스는 이메일 인증이 완료된 후 사용할 수 있습니다.
         <br />
         가입한 이메일로 인증을 완료해주세요.
       </p>
-      <Link href={'/'}>
-        <Button text="홈으로 가기" />
-      </Link>
-      <button
-        type="button"
+      <Anchor href={'/'} text="홈으로 가기" />
+      <Button
         onClick={emailVerifiedClick}
-        className="inline-block bg-indigo-600 px-5 py-3 rounded-sm text-sm text-white mt-16 ml-2 disabled:bg-indigo-200"
         disabled={loading}
-      >
-        이메일 재전송
-      </button>
+        text="이메일 재전송"
+      />
     </div>
   );
 };
