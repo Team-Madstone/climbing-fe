cc00d632c072ea437daa10241a61bc368daf498f
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-28 16:09:13 +0900

    🏷️ 버튼 타입 추가

--- components/button.tsx
+++ components/button.tsx
@@ -1,12 +1,17 @@
 interface ButtonProps {
+  type?: 'submit' | 'button';
   text: string;
   onClick?: () => void;
 }
 
-export default function Button({ text, onClick }: ButtonProps) {
+export default function Button({
+  type = 'button',
+  text,
+  onClick,
+}: ButtonProps) {
   return (
     <button
-      type="button"
+      type={type}
       onClick={onClick}
       className="px-5 py-3 bg-indigo-600 text-white rounded-sm text-sm cursor-pointer"
     >
