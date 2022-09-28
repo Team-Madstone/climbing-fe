717ead484937989390a887fba0f96da74e37a64d
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-29 00:14:22 +0900

    ✨ Button 컴포넌트 disabled props 추가

--- components/button.tsx
+++ components/button.tsx
@@ -1,18 +1,21 @@
-interface ButtonProps {
+interface IProps {
   type?: 'submit' | 'button';
   text: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
 export default function Button({
   type = 'button',
   text,
   onClick,
-}: ButtonProps) {
+  disabled,
+}: IProps) {
   return (
     <button
       type={type}
       onClick={onClick}
+      disabled={disabled}
       className="px-5 py-3 bg-indigo-600 text-white rounded-sm text-sm cursor-pointer"
     >
       {text}
