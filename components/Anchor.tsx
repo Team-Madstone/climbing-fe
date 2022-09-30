ed154b46afce7fb7fc3cfafc448b5e538723e71a
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-09-30 18:39:56 +0900

    ✨ anchor 컴포넌트 props 추가

--- components/Anchor.tsx
+++ components/Anchor.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link';
+import { classNames } from '../shared/share';
 
 interface IProps {
   href: string;
   text: string;
+  styling?: 'textOnly' | 'button' | 'fullLightBtn';
 }
 
-export default function Anchor({ href, text }: IProps) {
+export default function Anchor({ href, text, styling }: IProps) {
   return (
     <Link href={href}>
-      <a className="inline-block px-5 py-3 bg-indigo-600 text-white rounded-sm text-sm cursor-pointer mr-2">
+      <a
+        className={classNames(
+          `inline-block px-5 py-3 bg-indigo-600 hover:bg-indigo-700 text-white rounded-sm text-sm cursor-pointer mr-2`,
+          styling === 'textOnly' &&
+            'bg-transparent text-gray-500 hover:text-gray-700 hover:bg-transparent',
+          styling === 'fullLightBtn' &&
+            'bg-indigo-50 hover:bg-indigo-100 w-full border text-center text-gray-600 mr-0'
+        )}
+      >
         {text}
       </a>
     </Link>
